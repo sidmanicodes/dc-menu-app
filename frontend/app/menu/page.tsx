c97cf8a9b71bd558367f561b9b8474b53a24b0f1
sidzmani <siddharthmmani@gmail.com>
2024-02-24 23:35:03 -0800

    Removed redirections

--- frontend/app/menu/page.tsx
+++ frontend/app/menu/page.tsx
@@ -4,16 +4,12 @@ import Selections from "./Selections";
 import FoodItemDisplay from "./FoodItemDisplay";
 import supabase from "../api/supabase";
 import { useState } from "react";
-import { redirect } from "next/navigation";
 
 interface Props {
   searchParams: { query: string };
 }
 
 const Menu = ({ searchParams: { query } }: Props) => {
-  // Redirect for now
-  redirect("./");
-
   const [selectedDC, setSelectedDC] = useState("Segundo");
   const [selectedDay, setSelectedDay] = useState(0);
   const [selectedMeal, setSelectedMeal] = useState("Breakfast");
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,7 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  redirect("./menu");
   return (
     <>
       <div className="hero min-h-screen bg-gradient-to-r from-black to-primary">
