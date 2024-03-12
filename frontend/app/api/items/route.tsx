d08c6fc8efdb0467304b36db9603a2ff7c418b6d
sidzmani <siddharthmmani@gmail.com>
2024-03-12 02:00:04 -0700

    Fixed request for GET to be NextRequest, NOT NextAPIRequest

--- frontend/app/api/items/route.tsx
+++ frontend/app/api/items/route.tsx
@@ -1,8 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import supabase from "../supabase";
-import { NextApiRequest, NextApiResponse } from "next";
 
-export async function GET(req: NextApiRequest) {
+export async function GET(req: NextRequest) {
   return NextResponse.json({ message: "ok" });
 }
 
