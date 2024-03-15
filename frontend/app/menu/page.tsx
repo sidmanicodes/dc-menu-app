35c2c532426ab85c34a9a2bca04a3ebbe993e082
sidzmani <siddharthmmani@gmail.com>
2024-03-14 17:14:59 -0700

    Got rid of old comments

--- frontend/app/menu/page.tsx
+++ frontend/app/menu/page.tsx
@@ -5,31 +5,10 @@ import FoodItemDisplay from "./FoodItemDisplay";
 import supabase from "../api/supabase";
 import { useState } from "react";
 
-interface Props {
-  searchParams: { query: string };
-}
-
-const Menu = ({ searchParams: { query } }: Props) => {
-  // Get data from supabase
-  // const { data } = await supabase
-  //   .from("food_items")
-  //   .select("*")
-  //   .eq("dc", dc)
-  //   .eq("date", day)
-  //   .eq("meal", meal);
-
-  // Annotate foodItems as being of type FoodItem[]
-  // const foodItems: FoodItem[] = data as FoodItem[];
-
-  // console.log(dc);
-
+const Menu = () => {
   const [selectedDC, setSelectedDC] = useState("Segundo");
   const [selectedDay, setSelectedDay] = useState("0");
   const [selectedMeal, setSelectedMeal] = useState("Breakfast");
-
-  // console.log(selectedDC);
-  // console.log(selectedDay);
-  // console.log(selectedMeal);
 
   return (
     <div className="space-y-10">
