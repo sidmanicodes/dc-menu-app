62fba60d7af8cdf48ca03ef03a13af9e6c745470
sidzmani <siddharthmmani@gmail.com>
2024-06-03 23:12:56 -0700

    Fixed footer

--- frontend/app/menu/Footer.tsx
+++ frontend/app/menu/Footer.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-black flex flex-row items-center justify-between px-20 py-10">
-      <p className="text-center text-white text-xl">
-        Made with ❤️ by{" "}
+    <div className="bg-black flex flex-row items-center sm:justify-between justify-center sm:px-20 px-8 py-10">
+      <p className="text-center text-white sm:text-xl text-sm">
+        Made by{" "}
         <span className="font-semibold">
           <a href="https://aggieworks.org/" target="_blank">
             Aggieworks
           </a>
         </span>
       </p>
-      <p className="text-center text-white text-xl">
+      <p className="text-center text-white sm:text-xl text-sm">
         Have any questions? Fill out{" "}
         <a
           href="https://airtable.com/appYxSI2HPTOwdEMn/shrhxNPYQR0pBfXE7"
